fa5f57291d13ef73dc7d80bec76e8bbc9edc92c8
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2021-02-04 12:18:07 +0900

    new_home과 home 합침

--- src/main/java/com/project/onepage/Controller/PageController.java
+++ src/main/java/com/project/onepage/Controller/PageController.java
@@ -16,22 +16,16 @@ public class PageController{
 	//로그인 안됐는데, 메인 페이지로 갈 때
 	@RequestMapping(value = "/home") // GET 방식으로 페이지 호출
 	public ModelAndView homeGo(HttpServletRequest request) throws Exception {
-	
-		HttpSession session = request.getSession();
 		ModelAndView mav = new ModelAndView();
-		if(session.getAttribute("login")!=null) //기존에 login이라는 세션 값이 존재할 경
-			mav.setViewName("new_home");
-		
-		else
-			mav.setViewName("home");
+		mav.setViewName("home");
         return mav;
 	}
 
 	//로그인 됐는데, 메인 페이지로 갈 때
-	@RequestMapping(value = "/new_home") // GET 방식으로 페이지 호출
-	public ModelAndView new_homeGo() throws Exception {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("new_home");
-		return mav;
-	}
+//	@RequestMapping(value = "/new_home") // GET 방식으로 페이지 호출
+//	public ModelAndView new_homeGo() throws Exception {
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("home");
+//		return mav;
+//	}
 }
