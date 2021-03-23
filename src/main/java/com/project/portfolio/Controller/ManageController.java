b459e73890118db72a215f3f32499c4288b0b5d6
jy20170526 <47997297+jy20170526@users.noreply.github.com>
2021-03-23 22:31:24 +0900

    managecontroller added

---
+++ src/main/java/com/project/portfolio/Controller/ManageController.java
@@ -0,0 +1,11 @@
+package com.project.portfolio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller public class ManageController { 
+	@RequestMapping(value = "/manage") 
+	public String home(){ 
+		return "manage"; 
+	} 
+}
